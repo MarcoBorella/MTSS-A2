34d1208eeabc74ec6dabb35872cd6f08d1162240
rlotto <122532969+rlotto@users.noreply.github.com>
2023-05-18 13:18:15 +0200

    Aggiunti i numeri fino a 500 - Close #12

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,51 +7,30 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
 
-  public static String convert(int number) throws IllegalArgumentException {
-    String s = "L'intero deve essere compreso tra 0 e 100";
-    if(number < 0 || number > 100) {
-        throw new IllegalArgumentException(s);
-    }
+    public static String convert(int number) throws IllegalArgumentException {
+        String s = "L'intero deve essere compreso tra 0 e 500";
+        if(number < 0 || number > 500) {
+            throw new IllegalArgumentException(s);
+        }
 
-    String roman = "";
+        String roman = "";
 
-    while (number >= 100) {
-      roman = roman + "C";
-      number = number - 100;
-    }
-    while (number >= 90) {
-        roman = roman + "XC";
-        number = number - 90; 
-    }
-    while (number >= 50) {
-      roman += "L";
-      number = number - 50;
-    }
-    while (number >= 40) {
-      roman += "XL";
-      number = number - 40;
-    }
-    while (number >= 10) {
-      roman = roman + "X";
-      number = number - 10;
-    }
-    while (number >= 9) {
-      roman = roman + "IX";
-      number = number - 9;
-    }
-    while (number >= 5) {
-      roman += "V";
-      number = number - 5;
-    }
-    while (number >= 4) {
-        roman += "IV";
-        number = number - 4;
-    }  
-    while(number >= 1){
-        roman = roman + "I";
-        number = number - 1;
-    }
+        String[] numeriRomani =
+         {"D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] numeriArabi =
+        {500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-    return roman;
-  }
+        int i=0;
+
+        while (number > 0) {
+            while (number >= numeriArabi[i]) {
+                
+                roman += numeriRomani[i];
+                number -= numeriArabi[i];
+            }
+            i++;
+        }
+
+        return roman;
+    }
 }
