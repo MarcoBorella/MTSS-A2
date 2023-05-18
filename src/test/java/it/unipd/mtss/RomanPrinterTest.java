467545b2498d0d28f515921a37dfe2cd2390670b
rlotto <122532969+rlotto@users.noreply.github.com>
2023-05-18 11:10:30 +0200

    Implementazione classi - Close #4

---
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////
+// Marco Borella 1204685
+// Riccardo Lotto 1204687
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+/**
+ * Unit test for simple App.
+ */
+
+ public class RomanPrinterTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
